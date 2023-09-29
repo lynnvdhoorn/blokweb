071780cfc0e03f07a1ed1805888e0fb3858a4050
lynnvdhoorn <144168703+lynnvdhoorn@users.noreply.github.com>
2023-09-29 09:11:57 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -32,3 +32,22 @@ toggle.addEventListener('change', function () {
   }
 });
 
+
+
+// JavaScript om de diavoorstelling te regelen
+const slideshowContainer = document.querySelector(".container");
+const articles = Array.from(slideshowContainer.querySelectorAll(".container article"));
+let currentIndex = 0;
+
+// Functie om de volgende afbeelding weer te geven
+function showNextSlide() {
+  articles[currentIndex].classList.remove("visible");
+  currentIndex = (currentIndex + 1) % articles.length;
+  articles[currentIndex].classList.add("visible");
+}
+
+// Stel een timer in om de volgende afbeelding elke 3 seconden weer te geven
+setInterval(showNextSlide, 3000);
+
+// Direct na het laden van de pagina, toon het eerste artikel
+articles[currentIndex].classList.add("visible");
